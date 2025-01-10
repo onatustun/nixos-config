5b2b2106766d14b61550aa980c21c5c0e905da73
onatustun <onatustun@gmail.com>
2025-01-10 12:24:01 +0000

    hyprlock

--- home-manager/modules/hypr/hyprlock.nix
+++ home-manager/modules/hypr/hyprlock.nix
@@ -9,6 +9,28 @@
     	hide_cursor = true;
     	no_fade_in = false;
       };
+
+    background = [
+      {
+        monitor = "";
+	background-color = "black";
+      }
+    ];
+
+    input-field = [
+      {
+        monitor = ""; 
+	size = "300, 60";
+	outline_thickness = 4;
+	dots_rounding = -1;
+	dots-size = 0.4;
+	placeholder_text = "";
+	fade_on_empty = false;
+	position = "0, -10";
+	halign = "center";
+	valign = "center";
+       }
+     ];
    };
   };
 }
