871f2b705dd4d6ad71ce727622a500f836d3d790
onatustun <onatustun@gmail.com>
2025-01-18 18:23:05 +0000

    fonts

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -6,11 +6,16 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
-  
-  fonts.packages = with pkgs; [
-    fira-code
-    font-awesome
-    nerdfonts
-    noto-fonts
-  ];
+ 
+  fonts = {
+    fontDir.enable = true;
+    fontconfig.enable = true;
+
+    packages = with pkgs; [
+      font-awesome
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.noto
+      nerd-fonts.fira-code
+    ];
+  };
 }
